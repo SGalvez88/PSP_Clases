19a248bf8d9a14aec88fedcf6b285e4e1031c69d
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2022-10-06 10:52:21 +0200

    clase 6oct part2

--- src/clase6oct.java
+++ src/clase6oct.java
@@ -4,15 +4,19 @@ public class clase6oct {
 
     /*.redirectOutput(file), .redirectError(file), redirectInpunt(file)*/
     public static void main(String[] args) {
-        
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        File fileSalida = new File("src\\salida.txt");
-        File fileError = new File("src\\error.txt");
+        Process proceso = null;
+        ProcessBuilder processBuilder = new ProcessBuilder("cmd");
+        File fileEntrada = new File("resources\\fichero.bat");
+        File fileSalida = new File("resources\\salida.txt");
+        File fileError = new File("resources\\error.txt");
 
+        processBuilder.redirectInput(fileEntrada);
         processBuilder.redirectOutput(fileSalida);
         processBuilder.redirectError(fileError);
 
         try {
+
+            proceso = processBuilder.start();
             
         } catch (Exception e) {
             e.printStackTrace();
